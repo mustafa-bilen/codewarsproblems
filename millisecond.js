1ea36ef5dc260290e28854e3a16af7ae206579eb
mustafa-bilen <mustafabilen6669@gmail.com>
2022-08-12 17:00:51 +0300

    update

---
+++ millisecond.js
@@ -0,0 +1,28 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+
+//çözüm
+
+function past(h, m, s){
+    return (h * 3600000) + (m*60000) + (s * 1000);
+}
+
+
+
+// 1 min 60000 ms
+// 1 sec 1000 ms
+// 1 hour 3600000 msh
